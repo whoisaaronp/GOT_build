f6c18f229713e632806498e8629613ed2b70c998
Paterson <a_paterson@tss-hr420-dm12.fc.ca>
2019-03-18 12:07:56 -0400

    added new files and code

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -7,16 +7,31 @@
 			video		= document.querySelector('video');
 
 	function openLightbox(){
+		// debugger;
+		let targetHouse = this.className.split(" ")[1];
+
+		// this give us a lowercase house name -> the second class on all of the shields
+		// is stark, baratheon
+		targetHouse = targetHouse.replace(targetHouse.charAt(0), 
+			targetHouse.splice(1));
+
+		let targetSource = `video/House-${targetHouse}.mp4`;
 		lightbox.classList.add('lightbox-on');
+		video.load();
+		video.play();
 
 	}
 
 	function closeLightbox(){
 		lightbox.classList.remove('lightbox-on');
+
+		// rewind the current video and pause it
+		video.currentTime = 0;
+		video.pause();
 	}
 
 	sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
 	// end the video 
-	video.addEventListener('ended', closeLightBox);
+	video.addEventListener('ended', closeLightbox);
 
 })();
